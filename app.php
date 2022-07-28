a7227f4cf920a86d2f0a508c2e6918a76e4aca67
JhanilleH <99005703+JhanilleH@users.noreply.github.com>
2022-07-27 20:15:47 -0500

    Add files via upload

---
+++ app.php
@@ -0,0 +1,19 @@
+<?php
+      $serverName = "localhost";
+      $userName = "USERS";
+      $password = "engine451q";
+      $dbName = "USERS";
+
+      $conn = mysqli_connect($serverName, $userName, $password, $dbName);
+
+      $em = $_POST['email'];
+      $dev = $_POST['dev_num'];
+      $user = $_POST['username'];
+      $pass = $_POST['password'];
+
+      $query = "Insert into AppSignup(email,dev_num,username,password) values('$em', '$dev', '$user', '$pass')";
+
+      $run = mysqli_query($conn,$query) or die(mysqli_error());
+
+
+?>   
